28b5c5e3476433fc8a714b602a085b5736c99cae
Joshua Powell <joshua@viable>
2019-02-27 20:05:48 -0500

    Removed existing Angular site

--- app/modules/app--environment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
- angular.module('config', [])
-
-.constant('environment', {name:'development',apiUrl:'http://127.0.0.1:5000',siteUrl:'http://127.0.0.1:9000',defaultURL:'/',clientId:''})
-
-;
